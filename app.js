c939d0f9bbf21f2a21c00d34c22ebe63625e3a85
Mazharrais <mazharmajo72@gmail.com>
2024-08-10 19:32:33 +0500

    add code

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var selectField = document.getElementById("selectField");
 var selectText =  document.getElementById("selectText");
 var options = document.getElementsByClassName("options");
 var list = document.getElementById("list");
+var arrowIcon = document.getElementById("arrowIcon");
 
 
 selectField.onclick = function(){
     list.classList.toggle("hide");
+    arrowIcon.classList.toggle("rotate")
     
 }
 
